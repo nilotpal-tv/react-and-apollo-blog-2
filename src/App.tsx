5f25ddba348c7c914e6a30770085f8e7ae369b4a
Nilotpal Deka <nilotpaldeka@techvariable.com>
2022-12-10 18:58:30 +0530

    Add chakra UI and google fonts.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { Text } from '@chakra-ui/react';
+
 const App = () => {
-  return <h1>Hello World!</h1>;
+  return <Text>Hello, World</Text>;
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ChakraProvider, CSSReset } from '@chakra-ui/react';
+
 import App from './App';
+import theme from './theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -8,6 +11,9 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <CSSReset />
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
