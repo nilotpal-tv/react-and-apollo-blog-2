baa8a27afb9bb47521303ee8e9ce337788ff3341
Nilotpal Deka <nilotpaldeka@techvariable.com>
2022-12-11 22:37:18 +0530

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import { Heading } from '@chakra-ui/react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import EpisodesPage from './pages/Episodes';
+import EpisodePage from './pages/Episodes/Episode';
+import HomePage from './pages/Home';
+import CharacterPage from './pages/Home/Character';
 
 const App = () => {
-  return <Heading>Hello, World</Heading>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/">
+          <Route index element={<HomePage />} />
+          <Route path=":id" element={<CharacterPage />} />
+        </Route>
+        <Route path="/episodes">
+          <Route index element={<EpisodesPage />} />
+          <Route path=":id" element={<EpisodePage />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
